ec14eb1d6ceff08ac86ed98d7f7d3d43414de196
bprzybytkowski <bprzybytkowski@gmail.com>
2017-02-09 19:04:18 +0100

    Test commit

--- app/src/main/java/com/soldiersofmobile/bookscanner/api/BookApi.java
+++ app/src/main/java/com/soldiersofmobile/bookscanner/api/BookApi.java
@@ -3,9 +3,12 @@ package com.soldiersofmobile.bookscanner.api;
 import com.soldiersofmobile.bookscanner.api.model.BooksResponse;
 
 import retrofit2.Call;
+import retrofit2.http.Query;
 
 public interface BookApi {
 
     //TODO add missing Retrofit annotations
-    Call<BooksResponse> getBook(String isbn);
+    Call<BooksResponse> getBook(@Query("query") String query);
+
+
 }
